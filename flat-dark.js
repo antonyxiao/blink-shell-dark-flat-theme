9a4aa2f09bd0a0e5c431ec50856667710cd903a7
antonyxiao <antonyxiao16@gmail.com>
2022-02-21 18:23:10 -0800

    first commit

---
+++ flat-dark.js
@@ -0,0 +1,26 @@
+black       = '#2c3e50';
+red         = '#e74c3c'; // red
+green       = '#27ae60'; // green
+yellow      = '#f09b12'; // yellow
+blue        = '#2981ba'; // blue
+magenta     = '#9044af'; // pink
+cyan        = '#1abc9c'; // cyan
+white       = '#ecf0f1'; // light gray
+lightBlack  = '#95a5a6'; // medium gray
+lightRed    = '#e74c3c'; // red
+lightGreen  = '#2ecc71'; // green
+lightYellow = '#e8ce64'; // yellow
+lightBlue   = '#3498da'; // blue
+lightMagenta= '#a983f7'; // pink
+lightCyan   = '#51b09d'; // cyan
+lightWhite  = '#bdc3c7'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [black, red, green, yellow,
+                  blue, magenta, cyan, white,
+                  lightBlack, lightRed, lightGreen, lightYellow,
+                  lightBlue, lightMagenta, lightCyan, lightWhite]);
+
+t.prefs_.set('cursor-color', '#7f8c8d');
+t.prefs_.set('foreground-color', '#ffffff');
+t.prefs_.set('background-color', '#1C1E1F');
